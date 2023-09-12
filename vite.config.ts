c813270f713494173ad1542510db3cbd7ff382dd
Mikaleb <mikaleb@live.fr>
2023-09-12 22:46:13 +0200

    fix(port): port fiw

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,18 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    watch: {
+      usePolling: true
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 5173
   }
 })
